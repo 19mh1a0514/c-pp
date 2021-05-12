69ed094271f434f9030c7d85c05a29622fd9180c
19mh1a0514 <69674062+19mh1a0514@users.noreply.github.com>
2021-05-12 11:05:01 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main();
+{
+    int n,count=0;i=1;
+    cin>>n;
+    for(i=2;i<=2;i++)
+    {
+        if(n%i==0)
+        {
+           count=1;
+           break
+           
+        }
+     }
+      
+      if(count==0)
+      {
+        cout<<"prime";
+      }
+      else
+      {
+        cout<<"not prime";
+      }
+      return o;
+}      
+      
+      
+      
+      
+      
+      
+      
+        
+         
